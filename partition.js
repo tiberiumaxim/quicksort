3c6e72b864eab621b504875436d154e8c8e929e1
Tiberiu Maxim <tiberiu@itgambit.com>
2017-03-11 20:56:01 +0200

    updated test to check interchange function and partition step 2

--- partition.js
+++ partition.js
@@ -1,6 +1,30 @@
+const interchange = require('./interchange');
+
 function partition (A, p, r) {
-	const x = A[p];
-	return r;
+	const pivot = A[p];
+	let i = p;
+	let j = r;
+	let step = 1;
+
+	while (/*i <= j*/ step <= 2) {
+		console.log('step', step, A, i, j);
+		while (A[j] > pivot) {
+			console.log(A[j], 'decreasing j');
+			j--;
+		}
+		while (A[i] < pivot) {
+			console.log(A[i], 'increasing i');
+			i++;
+		}
+		if (i < j) {
+			interchange(A, i, j);
+			j--;
+			i++;
+		}
+		step++;
+	}
+
+	return j;
 }
 
 module.exports = partition;
--- test/test.js
+++ test/test.js
@@ -1,12 +1,30 @@
 const assert = require('assert');
 const _ = require('lodash');
 const partition = require('./../partition');
+const interchange = require('./../interchange');
 
+describe('Interchange', function () {
+	const arr = [5, 3, 2, 6, 4, 1, 3, 7];
+	interchange(arr, 0, 1);
+	interchange(arr, 2, 7);
+
+	it ('should interchange arr values', function () {
+		assert.equal(arr[0], 3);
+		assert.equal(arr[1], 5);
+		assert.equal(arr[2], 7);
+		assert.equal(arr[7], 2);
+	});
+});
 
 describe('Partition', function () {
 	const arr = [5, 3, 2, 6, 4, 1, 3, 7];
-	it ('should partition correctly', function () {
+	it ('should partition correctly | step 1', function () {
 		const q = partition(arr, 0, arr.length - 1);
 		assert.equal(q, 4);
+	});
+
+	it ('should partition correctly | step 2', function () {
+		const q = partition(arr, 0, 4);
+		assert.equal(q, 1);
 	});
 });
